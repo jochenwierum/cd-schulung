518174206eb4b2e6051fa911594a94189bb71148
Jochen Wierum <jochen@jowisoftware.de>
2014-08-27 15:23:47 +0200

    fixed sonar warnings

--- src/main/java/de/cologneintelligence/schulung/App.java
+++ src/main/java/de/cologneintelligence/schulung/App.java
@@ -1,8 +1,8 @@
 package de.cologneintelligence.schulung;
 
-public class App 
-{
-	public int add(int a, int b) {
-		return a + b;
-	}
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
